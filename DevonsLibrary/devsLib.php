cad2d594985fc6500a15011e7532a33ff60eee24
Devon O'Shaughnessy <devonosh@gmail.com>
2016-12-10 15:13:33 -0600

    Trying devsLib.php again

--- DevonsLibrary/devsLib.php
+++ DevonsLibrary/devsLib.php
@@ -1,3 +1,17 @@
+<?php
+    //open connection to mysql db
+    $connection = mysql_connect("localhost","root","P@ssw0rd");
+    if(!$connection)
+        die("Can't connect");
+
+    $db = mysql_select_db("lend_lib");
+    if(!$db)
+        die("Can't open the database");
+    //fetch table rows from mysql db
+    $sql = "select * from book";
+    $result = mysql_query($sql);
+?>
+
 <html>
 	<head>
 		<title>Devon's Library</title>
@@ -21,34 +35,19 @@
 			<button id="searchBtn" class="btn btn-default">Search</button>
 		</div>
 	</div>
-
-	<?php
-    //open connection to mysql db
-    $connection = mysql_connect("localhost","root","P@ssw0rd");
-    if(!$connection)
-        die("Can't connect");
-
-    $db = mysql_select_db("lend_lib");
-    if(!$db)
-        die("Can't open the database");
-    //fetch table rows from mysql db
-    $sql = "select * from book";
-    $result = mysql_query($sql);
-    ?>
-
 	<div class="container">
 		<div class="col-sm-10 offset-sm-2" id="listCol">
 			<div class="row">
 				<?php while($result = mysql_fetch_array($ResultSet, MYSQL_ASSOC))	
 					{
-					echo "<div class='col-xs-3'>"
-						"<img width='90' height='100' src=".$result["imageURL"].">
+					echo "<div class='col-xs-3'>
+						<img width='90' height='100' src=".$result["imageURL"].">
 					</div>
 					<div class='col-xs-9'>
 						
 						
 						 <span class='title'>".$result["title"]."</span><br />
-						<span class='author'>".$result["authorFName"]. .$result["authorLName"]."</span>
+						<span class='author'>".$result["authorFName"]."</span>
 						
 						
 					</div>"
